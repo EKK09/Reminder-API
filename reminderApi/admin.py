b0f09c8e7da9239df58017831d6550cf0a8d36ae
EKK <xe856423@gmail.com>
2020-05-16 20:18:05 +0800

    新增提醒事項類別 mode、migration

--- reminderApi/admin.py
+++ reminderApi/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from reminderApi.models import ReminderTag
 
 # Register your models here.
+admin.site.register(ReminderTag)
--- reminderApi/models.py
+++ reminderApi/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ReminderTag(models.Model):
+    name = models.CharField(max_length=20)
+    created_date = models.DateTimeField('建立時間', auto_now_add=True)
