82a09b4b46c7e03296222f94d99ece5da56e30e3
EKK <xe856423@gmail.com>
2020-07-31 20:51:38 +0800

    新增取得、新增、刪除提醒事項分類 API

--- reminderApi/api.py
+++ reminderApi/api.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('tags', views.tags),
+    path('categorys', views.categorys),
 ]
--- reminderApi/views.py
+++ reminderApi/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse, HttpResponse
 from .models.ReminderTagModel import ReminderTag
+from .models.ReminderCategoryModel import ReminderCategory
 from django.views.decorators.csrf import csrf_exempt
 import json
 
@@ -26,3 +27,26 @@ def tags(request):
     	ReminderTag.deleteTag(bodyJson['id'])
 
     	return HttpResponse(status=204)
+
+@csrf_exempt
+def categorys(request):
+    method = request.method
+
+    if method == 'GET':
+        content = ReminderCategory.getAll()
+
+        return JsonResponse(content, safe=False, charset='utf-8')
+
+    elif method == 'POST':
+        bodyUnicode = request.body.decode('utf-8')
+        bodyJson = json.loads(bodyUnicode)
+        ReminderCategory.add(bodyJson['name'])
+
+        return HttpResponse(status=204)
+
+    elif method == 'DELETE':
+        bodyUnicode = request.body.decode('utf-8')
+        bodyJson = json.loads(bodyUnicode)
+        ReminderCategory.deleteCategory(bodyJson['id'])
+
+        return HttpResponse(status=204)
