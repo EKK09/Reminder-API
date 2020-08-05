070a218e4b7eab122c8f3709c1ec1b3fca2a9531
EKK <xe856423@gmail.com>
2020-08-05 22:53:27 +0800

    刪除 model 內無用方法

--- reminderApi/models/ReminderModel.py
+++ reminderApi/models/ReminderModel.py
@@ -11,27 +11,4 @@ class Reminder(models.Model):
     created_date = models.DateTimeField(
         '建立時間', auto_now_add=True)
 
-    # TODO: 新增標籤與類別
-
-    @staticmethod
-    def getAll():
-        reminders = []
-        for querySet in Reminder.objects.all():
-            reminder = {'id': querySet.id, 'title': querySet.title,
-                        'remindTime': querySet.remind_time, 'finished': querySet.finished}
-            reminders.append(reminder)
-
-        return reminders
-
-    @staticmethod
-    def add(reminder):
-        unit = Reminder.objects.create(
-            title=reminder['title'], remind_time=reminder['remindTime'], finished=reminder['finished'])
-        unit.save()
-        return
-
-    @staticmethod
-    def deleteReminder(id):
-        unit = Reminder.objects.get(id=id)
-        unit.delete()
-        return
+    # TODO: 新增標籤
--- reminderApi/models/ReminderTagModel.py
+++ reminderApi/models/ReminderTagModel.py
@@ -1,26 +1,7 @@
 from django.db import models
 
+
 class ReminderTag(models.Model):
     name = models.CharField(max_length=20)
-    created_date = models.DateTimeField('建立時間', auto_now_add=True)
-
-    @staticmethod
-    def getAll():
-        tags = []
-        for querySet in ReminderTag.objects.all():
-            tag = {'id': querySet.id, 'name': querySet.name}
-            tags.append(tag)
-        return tags
-
-    @staticmethod
-    def add(name):
-        unit = ReminderTag.objects.create(name=name)
-        unit.save()
-        return
-
-    @staticmethod
-    def deleteTag(id):
-        unit = ReminderTag.objects.get(id = id)
-        unit.delete()
-        return
-
+    created_date = models.DateTimeField(
+        '建立時間', auto_now_add=True)
